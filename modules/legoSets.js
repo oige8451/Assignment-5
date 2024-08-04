c4421a489a08ad94aefb03fa20719c1aaf98e942
ooige1 <oige8451@gmail.com>
2024-08-03 21:00:08 -0400

    Update legoSets.js

--- modules/legoSets.js
+++ modules/legoSets.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
+const pg = require('pg'); 
 
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
